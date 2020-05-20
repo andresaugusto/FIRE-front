50fea45af66531bf02820cce5c23a279d1c66831
billyh <billyhkaufman@outlook.com>
2020-05-19 22:05:22 -0400

    built modal

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-const Home = () => (
-	<>
-		<header className='home-header'>
-			<div className='home-header-text'>
-				<h1>
-					welcome to <span className='home-header-logo'>f i r e</span>
-				</h1>
-				<h2>{'explore \u00b7 edit \u00b7 create'}</h2>
-			</div>
-		</header>
-	</>
-);
+const Home = () => {
+	return (
+		<>
+			<header className='home-header'>
+				<div className='home-header-text'>
+					<h1>
+						welcome to <span className='home-header-logo'>f i r e</span>
+					</h1>
+					<h2>{'explore \u00b7 edit \u00b7 create'}</h2>
+				</div>
+			</header>
+		</>
+	);
+};
 
 export default Home;
