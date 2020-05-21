24c804e227dbfffeb1116e9261545efa5b70342e
andresaugusto-ga <andres.augusto.ga@icloud.com>
2020-05-21 17:47:00 -0400

    Updates

---
+++ precache-manifest.e33d394970684086dc6540742023d01a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434e106300c0005eb1d5f58fa78950be",
+    "url": "/andresaugusto/FIRE-front/index.html"
+  },
+  {
+    "revision": "c53b77af6d2f2d5e3462",
+    "url": "/andresaugusto/FIRE-front/static/css/main.e5aff868.chunk.css"
+  },
+  {
+    "revision": "6de16938df9adae7939c",
+    "url": "/andresaugusto/FIRE-front/static/js/2.9ba9eb6e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/andresaugusto/FIRE-front/static/js/2.9ba9eb6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c53b77af6d2f2d5e3462",
+    "url": "/andresaugusto/FIRE-front/static/js/main.a7d56594.chunk.js"
+  },
+  {
+    "revision": "37935014c21d6d63172e",
+    "url": "/andresaugusto/FIRE-front/static/js/runtime-main.adad8ec6.js"
+  }
+]);
